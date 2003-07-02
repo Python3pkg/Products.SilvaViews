93e704ea63466d32e7ad6ca8e85e20c9f52a471c
Martijn Faassen <faassen@startifact.com>
2003-07-02 11:44:41 +0000

    Initial revision

---
+++ __init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2002, 2003 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Revision: 1.1 $
+
+import ViewRegistry, MultiViewRegistry
+
+def initialize(context):
+    context.registerClass(
+        ViewRegistry.ViewRegistry,
+        constructors = (ViewRegistry.manage_addViewRegistryForm,
+                        ViewRegistry.manage_addViewRegistry),
+        icon = "www/silva_view_registry.gif"
+        )
+
+    context.registerClass(
+        MultiViewRegistry.MultiViewRegistry,
+        constructors = (MultiViewRegistry.manage_addMultiViewRegistryForm,
+                        MultiViewRegistry.manage_addMultiViewRegistry),
+        icon = "www/silva_multi_view_registry.gif"
+        )
+
+
+
