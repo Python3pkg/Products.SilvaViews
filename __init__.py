2fd31d21d1d07cd2d6a6778c689a2fd6b4246bb0
Sylvain Viollon <thefunny@gmail.com>
2010-02-22 15:50:21 +0000

    Fix copyright headers / svn:keywords.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-# Copyright (c) 2002-2008 Infrae. All rights reserved.
+# Copyright (c) 2002-2010 Infrae. All rights reserved.
 # See also LICENSE.txt
-# $Revision: 1.2 $
+# $Revision$
 
 import ViewRegistry, MultiViewRegistry
 
